1c492346d612d758dd5c9178aa16b943227abaf8
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 19:59:00 -0400

    [harman]: Employee class\

--- src/main/java/payroll/Employee.java
+++ src/main/java/payroll/Employee.java
@@ -18,41 +18,50 @@ public abstract class Employee implements IPrintable {
     }
 
     public Employee(String name, int age, Vehicle vehicle) {
-        this.name=name;
-        this.age=calcBirthYear(age);
+        this.name = name;
+        this.age = calcBirthYear(age);
         setVehicle(vehicle);
     }
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
+
     public int calcBirthYear(int yearofBirth) {
         //yearofBirth=getAge();
-        LocalDate end =  LocalDate.now();
-        int age=Integer.valueOf(end.toString().substring(0, 4))  ;
-        age=age-yearofBirth-1;
+        LocalDate end = LocalDate.now();
+        int age = Integer.valueOf(end.toString().substring(0, 4));
+        age = age - yearofBirth - 1;
         return age;
     }
-    public double calcEarnings(){
-        return this.earnings=1000;
+
+    public double calcEarnings() {
+        return this.earnings = 1000;
 
     }
+
     public Vehicle getVehicle() {
         return vehicle;
-    }public void setVehicle(Vehicle vehicle) {
+    }
+
+    public void setVehicle(Vehicle vehicle) {
         this.vehicle = vehicle;
     }
 
     @Override
     public String printMyData() {
-        return "Employee Name: "+getName()+"\n"+"Employee Age"+getAge();
+        return "Employee Name: " + getName() + "\n" + "Employee Age" + getAge();
     }
+}
