4c7d2610ddb650ee191b7c44e8c6b2b353c077b4
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 21:18:31 -0400

    [Charan] : Intern class\checkvehicle logic

--- src/main/java/com/lambton/Intern.java
+++ src/main/java/com/lambton/Intern.java
@@ -30,6 +30,28 @@ public class Intern
         this.vehicle = vehicle;
     }
 
+    private void checkVehicle(StringBuilder stringBuilder) {
+        if (vehicle == null) {
+            stringBuilder.append("Employee has no vehicle registerd");
+        } else if (vehicle instanceof Car) {
+            stringBuilder.append("Employee has a Car\n")
+                    .append(" -Make: ").append(getVehicle().getMake())
+                    .append("\n -Plate: ").append(getVehicle().getPlate())
+                    .append("\n -Color: ").append(getVehicle().getColor())
+                    .append("\n -Distance Travelled: ").append(((Car)getVehicle()).getDistanceTravelled())
+                    .append("\n -Capacity: ").append(((Car)getVehicle()).getCapacity());
+
+        } else if (vehicle instanceof Motorcycle) {
+            stringBuilder.append("Employee has a MotorCycle\n")
+                    .append(" -Make: ").append(getVehicle().getMake())
+                    .append("\n -Plate: ").append(getVehicle().getPlate())
+                    .append("\n -Color: ").append(getVehicle().getColor())
+                    .append("\n -Distance Travelled: ").append(((Motorcycle)getVehicle()).getDistanceTravelled())
+            ;
+        }
+    }
+
+
     public Intern(String schoolName, double earnings, Vehicle vehicle) {
         this.schoolName = schoolName;
         this.earnings = earnings;
