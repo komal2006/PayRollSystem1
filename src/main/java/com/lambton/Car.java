b6de9ff22762de86c3153a76297f0dae853328a9
CHARAN <charanpreetkaur026@gmail.com>
2019-10-05 14:48:08 -0400

    [komal]  constructor for car class

--- src/main/java/com/lambton/Car.java
+++ src/main/java/com/lambton/Car.java
@@ -4,6 +4,11 @@ public class Car extends Vehicle {
 
     private Double distanceTravelled;
     private String capacity;
-    
+    public Car(String make, String color, String plate, Double distanceTravelled, String capacity) {
+        super(make, color, plate);
+        this.distanceTravelled=distanceTravelled;
+        this.capacity=capacity;
+
+    }
 
 }
