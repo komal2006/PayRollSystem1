cc100e75bd33b9b238a99a5166c14e4f6626a804
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 20:55:17 -0400

    [Varinder]:fullTime created\ setter and getter

--- src/main/java/com/lambton/FullTime.java
+++ src/main/java/com/lambton/FullTime.java
@@ -6,4 +6,27 @@ package com.lambton;
         private double bonus;
         private Vehicle vehicle;
 
-}
+        public double getSalary() {
+            return salary;
+        }
+
+        public void setSalary(double salary) {
+            this.salary = salary;
+        }
+
+        public double getBonus() {
+            return bonus;
+        }
+
+        public void setBonus(double bonus) {
+            this.bonus = bonus;
+        }
+
+        public Vehicle getVehicle() {
+            return vehicle;
+        }
+
+        public void setVehicle(Vehicle vehicle) {
+            this.vehicle = vehicle;
+        }
+    }
