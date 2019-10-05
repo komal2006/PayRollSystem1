758e23a7d60b8b5dd2e8e6d90574bf2d96424cf3
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 20:33:45 -0400

    [Varinder]:Employee class with constructors

--- src/main/java/payroll/Employee.java
+++ src/main/java/payroll/Employee.java
@@ -8,6 +8,39 @@ public abstract class Employee {
     private int age;
     private Vehicle vehicle;
     private double earnings;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public double getEarnings() {
+        return earnings;
+    }
+
+    public void setEarnings(double earnings) {
+        this.earnings = earnings;
+    }
 }
 
 
