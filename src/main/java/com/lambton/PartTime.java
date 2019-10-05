8d876cbd3e4b8644b3bf1e2bc5516e1d64164f7d
CHARAN <charanpreetkaur026@gmail.com>
2019-10-05 14:36:33 -0400

    [charan]: Rebuilding PartTime class\ constructor

--- src/main/java/com/lambton/PartTime.java
+++ src/main/java/com/lambton/PartTime.java
@@ -4,5 +4,10 @@ public class PartTime
 {
     private double rate;
     private float hoursWorked;
+    public PartTime(String name, int age,float rate,float hoursWorked,Vehicle vehicle){
+        super(name,age,vehicle);
+        this.rate=rate;
+        this.hoursWorked=hoursWorked;
+    }
 
 }
