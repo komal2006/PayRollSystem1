91fa44e5353a2c098b28353902436f8d70cf1a7a
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 21:21:58 -0400

    [Charan] : Commission class\setter and getter

--- src/main/java/com/lambton/CommissionBasedPartTime.java
+++ src/main/java/com/lambton/CommissionBasedPartTime.java
@@ -5,4 +5,19 @@ public class CommissionBasedPartTime {
     private double commissionPerc;
     private Vehicle vehicle;
 
+    public double getCommissionPerc() {
+        return commissionPerc;
+    }
+
+    public void setCommissionPerc(double commissionPerc) {
+        this.commissionPerc = commissionPerc;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
 }
