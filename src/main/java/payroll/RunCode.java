2bfb824a33b55c3a7a03ab3579db59a887daf1c5
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 20:06:48 -0400

    [harman]: Main class\declare intern detalis

--- src/main/java/payroll/RunCode.java
+++ src/main/java/payroll/RunCode.java
@@ -9,5 +9,11 @@ public class RunCode {
         Car car1= new Car("Challenger","Black","CAMY",43000.0,"6");
         CommissionBasedPartTime commissionBasedPartTime= new CommissionBasedPartTime("Harman", 1994, 40.0f,10.0f, 40,bike);
         FixedBasedPartTime fixedBased=new FixedBasedPartTime("Charan",1993,20.0f,9.0f,300,null);
+        Intern intern=new Intern("Varinder",1994,"Cambrian College",car1);
+
+
+
+
+
     }
 }
