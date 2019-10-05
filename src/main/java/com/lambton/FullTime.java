0e3781c17cf7296608938f8e80a91a5c56f9710f
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 20:56:26 -0400

    [Varinder]:fullTime created\ constructor

--- src/main/java/com/lambton/FullTime.java
+++ src/main/java/com/lambton/FullTime.java
@@ -6,6 +6,14 @@ package com.lambton;
         private double bonus;
         private Vehicle vehicle;
 
+        public FullTime(String name, int age, double salary,double bonus,Vehicle vehicle) {
+            super(name, age,vehicle);
+            this.salary=salary;
+            this.bonus=bonus;
+            setVehicle(vehicle);
+        }
+
+
         public double getSalary() {
             return salary;
         }
