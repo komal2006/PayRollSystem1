9bb07eecc45c3bb67e9074fb1781c4353b71fb55
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 20:03:09 -0400

    [harman]: Main class\declare motorcycle detalis

--- src/main/java/payroll/RunCode.java
+++ src/main/java/payroll/RunCode.java
@@ -1,9 +1,11 @@
 package payroll;
 
-public class RunCode
-{
-    public static double totalPayroll=0;
-    public static void main(String []args){
+public class RunCode {
+    public static double totalPayroll = 0;
 
+    public static void main(String[] args) {
+
+        Motorcycle bike = new Motorcycle("Kawasaki", "Red", "Deep", 3000.0);
 
     }
+}
