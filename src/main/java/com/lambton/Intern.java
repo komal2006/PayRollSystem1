884acadf6235eaff280610a363ad4eabaec6e4ce
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 21:16:41 -0400

    [Charan] : Intern class\setter and getter

--- src/main/java/com/lambton/Intern.java
+++ src/main/java/com/lambton/Intern.java
@@ -6,4 +6,27 @@ public class Intern
     private double earnings;
     private Vehicle vehicle;
 
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public double getEarnings() {
+        return earnings;
+    }
+
+    public void setEarnings(double earnings) {
+        this.earnings = earnings;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
 }
