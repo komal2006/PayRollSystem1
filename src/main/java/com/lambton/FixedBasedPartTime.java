b0be11ee78d8e1f5c7a9e4b70e09e2286e924a11
CHARAN <charanpreetkaur026@gmail.com>
2019-10-05 14:12:33 -0400

    [charan]: Rebuilding FixedBasedPartTime class\ declaring constructor

--- src/main/java/com/lambton/FixedBasedPartTime.java
+++ src/main/java/com/lambton/FixedBasedPartTime.java
@@ -4,6 +4,10 @@ public class FixedBasedPartTime
 {
     double fixedAmount ;
     double totalEarnings;
+    public FixedBasedPartTime(String name, int age, float rate, float hoursWorked, double fixedAmount) {
+        super(name, age, rate, hoursWorked);
+        this.fixedAmount = fixedAmount;
+    }
 
 
 }
