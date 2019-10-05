9625076192070a8e050caeaaa7225b80f6929e38
CHARAN <charanpreetkaur026@gmail.com>
2019-10-04 21:12:09 -0400

    [Charan] : Vehicle class created\setter and getter

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -6,4 +6,28 @@ public class Vehicle
     private String make;
     private String color;
     private String plate;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
 }
